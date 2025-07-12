6efbc53eca828891d44c48c384d1121f5995d702
LamaV10 <lama.customs@gmail.com>
2025-07-12 18:00:37 +0200

    Renamed Source to Src.

---
+++ Src/CMakeFiles/CppSTK.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppSTK.dir/link.d"
+  "CMakeFiles/CppSTK.dir/main.cpp.o"
+  "CMakeFiles/CppSTK.dir/main.cpp.o.d"
+  "CppSTK"
+  "CppSTK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppSTK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
